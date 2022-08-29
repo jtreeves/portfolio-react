124afdaa1cfc25b43760e80f3ab3b3786785bce2
jtreeves <jackson.t.reeves@gmail.com>
2022-08-28 22:00:43 -0400

    [About] Fill out with details from person object

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,12 @@
+import person from '../data/person'
+
 function About() {
     return (
         <main>
             <h1>About</h1>
+            <img src={person.photo} alt={person.name} />
+            <h2>{person.name}</h2>
+            <p>{person.bio}</p>
         </main>
     )
 }
