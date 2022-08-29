4c7c404149f10dd2dcefe575739ddc3c69ca50b0
jtreeves <jackson.t.reeves@gmail.com>
2022-08-28 22:03:50 -0400

    [Contact] Add basic call to action; link to email

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,6 +2,7 @@ function Contact() {
     return (
         <main>
             <h1>Contact</h1>
+            <p>If you'd like collaborate on an interesting project or just chat code, feel free to hit me up via <a href='mailto:jr@jacksonreeves.com'>email</a></p>
         </main>
     )
 }
