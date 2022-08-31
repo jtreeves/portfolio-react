44e6b68e2f40d77bbd899fe6671fef32a6286259
jtreeves <jackson.t.reeves@gmail.com>
2022-08-31 17:13:09 -0400

    [data] Add types to projects; turn bio into array of paragraphs for person

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,7 @@
 const projects = [
     {
         title: 'Kaleidoscope',
+        type: 'app',
         description: 'Budget app that lets users compare different possible expense plans',
         image: 'https://i.imgur.com/vKOdCf9.png',
         repositoryLink: 'https://github.com/jtreeves/budget-frontend',
@@ -8,13 +9,15 @@ const projects = [
     },
     {
         title: 'Math Attack!',
+        type: 'app',
         description: 'Game to test your knowledge with basic arithmetic operations',
         image: 'https://i.imgur.com/XUexFZD.png',
         repositoryLink: 'https://github.com/jtreeves/vue-learn-math',
-        deploymentLink: 'https://vue-math-attack.netlify.app/home'
+        deploymentLink: 'https://vue-math-attack.netlify.app/'
     },
     {
         title: 'Regressions',
+        type: 'library',
         description: 'Library to generate statistical regressions models from provided data sets',
         image: 'https://i.imgur.com/NXAcCbT.png',
         repositoryLink: 'https://github.com/jtreeves/regressions_library',
@@ -22,6 +25,7 @@ const projects = [
     },
     {
         title: 'Salesforce Certification Calculator',
+        type: 'library',
         description: 'Module to determine the cumulative score for Salesforce exams',
         image: 'https://i.imgur.com/kH9ITXj.png',
         repositoryLink: 'https://github.com/jtreeves/salesforce_certification_calculator',
