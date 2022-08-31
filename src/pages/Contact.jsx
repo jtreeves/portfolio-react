2ad2feb6c52ea14260c38d8d6d373e477409897a
jtreeves <jackson.t.reeves@gmail.com>
2022-08-31 17:17:45 -0400

    [Contact] Add period to end of sentence for consitency with Resume

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,7 +4,7 @@ function Contact() {
             <h1>Contact</h1>
 
             <p>
-                If you'd like collaborate on an interesting project or just chat code, feel free to hit me up via <a href='mailto:jr@jacksonreeves.com'>email</a>
+                If you'd like collaborate on an interesting project or just chat code, feel free to hit me up via <a href='mailto:jr@jacksonreeves.com'>email</a>.
             </p>
         </main>
     )
