3f82d0968fb5bf8a3fcc20acc0b286f5ee8c429c
jtreeves <jackson.t.reeves@gmail.com>
2022-08-30 20:59:42 -0400

    [Home] Add FontAwesome icons for sections of site

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {  faBriefcase, faComment, faPaperPlane, faFileAlt } from '@fortawesome/free-solid-svg-icons'
+
+
 function Home() {
     return (
         <main>
             <h1>Home</h1>
+            <FontAwesomeIcon icon={faComment} size='6x' />
+            <FontAwesomeIcon icon={faPaperPlane} size='6x' />
+            <FontAwesomeIcon icon={faBriefcase} size='6x' />
+            <FontAwesomeIcon icon={faFileAlt} size='6x' />
         </main>
     )
 }
