c400464ad41a0e245cbdaec36c383cb721f21426
jtreeves <jackson.t.reeves@gmail.com>
2022-08-30 23:28:37 -0400

    [ProjectDetails] Display project info; use findProject helper

--- src/pages/ProjectDetails.jsx
+++ src/pages/ProjectDetails.jsx
@@ -1,7 +1,39 @@
+import { useLocation } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
+import findProject from '../utilities/findProject'
+
 function ProjectDetails() {
+    const location = useLocation()
+    const path = location.pathname
+    const project = findProject(path)
+
     return (
         <main>
-            <h1>ProjectDetails</h1>
+            <h1>{project.title}</h1>
+
+            <img src={project.image} alt={project.title} />
+
+            <p>{project.description}</p>
+
+            <a 
+                href={project.deploymentLink}
+                target='_blank'
+                rel='noreferrer'
+            >
+                <Button variant='primary'>
+                    View deployed site
+                </Button>
+            </a>
+
+            <a 
+                href={project.repositoryLink}
+                target='_blank'
+                rel='noreferrer'
+            >
+                <Button variant='primary'>
+                    View code
+                </Button>
+            </a>
         </main>
     )
 }
