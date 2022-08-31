bd0e9f7090c48ebcaac4c723d8acc32a694da31d
jtreeves <jackson.t.reeves@gmail.com>
2022-08-30 23:04:28 -0400

    [Projects] Display ProjectsList and pass it projects data as prop

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,12 @@
+import ProjectsList from '../components/ProjectsList'
+import projects from '../data/projects'
+
 function Projects() {
     return (
         <main>
             <h1>Projects</h1>
+
+            <ProjectsList projects={projects} />
         </main>
     )
 }
