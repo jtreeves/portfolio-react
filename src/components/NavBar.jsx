d4cb1020755f46100f88d9430a9e2bc94ecd2617
jtreeves <jackson.t.reeves@gmail.com>
2022-08-31 20:29:42 -0400

    [NavBar] Remove expansion and toggling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,11 @@
-import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Container, Navbar as BootstrapNavBar, Nav } from 'react-bootstrap'
 import person from '../data/person'
 
 function NavBar() {
-    const [expanded, setExpanded] = useState(false)
-
     return (
         <BootstrapNavBar 
-            expanded={expanded} 
             bg='light'
-            expand='lg'
-            sticky='top'
         >
             <Container fluid>
                 <Link 
@@ -21,51 +15,35 @@ function NavBar() {
                     {person.name}
                 </Link>
 
-                <BootstrapNavBar.Toggle
-                    aria-controls='navbarScroll'
-                    onClick={() => setExpanded(expanded ? false : 'expanded')}
-                />
-
-                <BootstrapNavBar.Collapse 
-                    id='navbarScroll'
-                >
-                    <Nav 
-                        className='me-auto my-2 my-lg-0' 
-                        navbarScroll
+                <Nav>
+                    <Link
+                        to='/about'
+                        className='nav-link'
                     >
-                        <Link
-                            to='/about'
-                            className='nav-link'
-                            onClick={() => setExpanded(false)}
-                        >
-                            About
-                        </Link>
-                        
-                        <Link
-                            to='/contact'
-                            className='nav-link'
-                            onClick={() => setExpanded(false)}
-                        >
-                            Contact
-                        </Link>
-                        
-                        <Link
-                            to='/resume'
-                            className='nav-link'
-                            onClick={() => setExpanded(false)}
-                        >
-                            Resume
-                        </Link>
-                        
-                        <Link
-                            to='/projects'
-                            className='nav-link'
-                            onClick={() => setExpanded(false)}
-                        >
-                            Projects
-                        </Link>
-                    </Nav>
-                </BootstrapNavBar.Collapse>
+                        About
+                    </Link>
+                    
+                    <Link
+                        to='/contact'
+                        className='nav-link'
+                    >
+                        Contact
+                    </Link>
+                    
+                    <Link
+                        to='/resume'
+                        className='nav-link'
+                    >
+                        Resume
+                    </Link>
+                    
+                    <Link
+                        to='/projects'
+                        className='nav-link'
+                    >
+                        Projects
+                    </Link>
+                </Nav>
             </Container>
         </BootstrapNavBar>
     )
