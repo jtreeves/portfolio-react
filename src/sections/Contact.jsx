e473075594f270840bd072503290d37a686c5561
jtreeves <jackson.t.reeves@gmail.com>
2022-10-08 16:46:53 -0400

    [Contact] Add form

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,4 +1,28 @@
+import { useRef } from 'react'
+import emailjs from '@emailjs/browser'
+
+const SERVICE_ID = process.env.REACT_APP_SERVICE_ID
+const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID
+const PUBLIC_KEY = process.env.REACT_APP_PUBLIC_KEY
+
 function Contact() {
+    const form = useRef()
+
+    const sendEmail = async (e) => {
+        e.preventDefault()
+    
+        const result = await emailjs.sendForm(
+            SERVICE_ID, 
+            TEMPLATE_ID, 
+            form.current, 
+            PUBLIC_KEY
+        )
+
+        console.log('RESULT TEXT: ', result.text)
+
+        e.target.reset()
+    }
+
     return (
         <section id='contact'>
             <h1>Contact</h1>
@@ -6,6 +30,16 @@ function Contact() {
             <p>
                 If you'd like collaborate on an interesting project or just chat code, feel free to hit me up via <a href='mailto:jr@jacksonreeves.com'>email</a>.
             </p>
+
+            <form ref={form} onSubmit={sendEmail}>
+                <label>Name</label>
+                <input type='text' name='name' required />
+                <label>Email</label>
+                <input type='email' name='email' required />
+                <label>Message</label>
+                <textarea name='message' required />
+                <input type='submit' value='Send' />
+            </form>
 
             <article>
                 Reach out to me on social media!
