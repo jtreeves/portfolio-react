3c9801f64263567216387b665f7bc33d813bb80d
jtreeves <jackson.t.reeves@gmail.com>
2022-10-09 13:18:21 -0400

    [findPreviousSection] Use or equal to 0 to ensure About not skipped

--- src/utilities/findPreviousSection.js
+++ src/utilities/findPreviousSection.js
@@ -4,7 +4,7 @@ function findPreviousSection(currentSection) {
     const currentIndex = sections.indexOf(currentSection)
     const previousIndex = currentIndex - 1
 
-    if (previousIndex > 0) {
+    if (previousIndex >= 0) {
         return sections[previousIndex]
     } else {
         return 'home'
