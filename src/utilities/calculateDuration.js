fca10622e56520e11643ba65f806bde27f9a1a14
jtreeves <jackson.t.reeves@gmail.com>
2022-10-13 18:01:31 -0400

    [calculateDuration] Convert ms to yrs

--- src/utilities/calculateDuration.js
+++ src/utilities/calculateDuration.js
@@ -2,9 +2,10 @@ function calculateDuration(startDate, endDate) {
     const endExists = !!endDate
     const now = new Date()
     const verifiedEndDate = endExists ? endDate : now
-    const duration = verifiedEndDate - startDate
+    const durationMs = verifiedEndDate - startDate
+    const durationYrs = durationMs / 31556952000
 
-    return duration
+    return durationYrs
 }
 
 export default calculateDuration
